27e575692804f70186ce153e076933d813a8bbb2
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-12-01 21:58:04 +0000

    todo update

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import numpy as np
 from utils import COLORS, load_image
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
-from matplotlib import
+from matplotlib import pyplot as
