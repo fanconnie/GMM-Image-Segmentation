8449ad71e9f07f82bdccd68edaad90c427feb310
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-12-02 15:52:53 +0000

    Fix Travis script

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import numpy as np
 from utils import COLORS, load_image
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
-from matplotlib import pyplot as
+from matplotlib import pyplot as plt
+
+
+clas
