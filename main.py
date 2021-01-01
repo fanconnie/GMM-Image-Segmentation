7504ca69e1735761e7c8c78ab7ceb369d5840407
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-01-01 15:06:32 +0000

    merge emacs-23

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ class GMM:
     def inference(self, datas): # E-step
         unnormalized_probs = []
         for i in range(self.ncomp):
-            mu, cov, prior =
+            mu, cov, prior = self.mus[i, :
