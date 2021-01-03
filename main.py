d43e0fe650f8463f138280a2f6462b46a4a078e8
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-01-03 16:22:23 +0000

    Scrutinizer fixes

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ class GMM:
     def inference(self, datas): # E-step
         unnormalized_probs = []
         for i in range(self.ncomp):
-            mu, cov, prior = self.mus[i, :], self.covs[i, :, :], s
+            mu, cov, prior = self.mus[i, :], self.covs[i, :, :], self.priors[i
