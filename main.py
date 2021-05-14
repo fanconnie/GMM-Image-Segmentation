2e1eea9edff17ae317f60648a02f14ed29364d06
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-05-14 10:23:17 +0000

    Added titles

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ class GMM:
         new_mus, new_covs, new_priors = [], [], []
         soft_counts = np.sum(beliefs, axis=0)
         for i in range(self.ncomp):
-            new_mu = n
+            new_mu = np.sum(np.
