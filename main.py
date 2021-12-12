e6f73cc897d058910be1d982ad838cf80764de8f
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-12-12 14:37:19 +0000

    ip: zero sockaddr returned on error queue

--- main.py
+++ main.py
@@ -42,4 +42,5 @@ class GMM:
             new_priors.append(soft_counts[i] / np.sum(soft_counts))
 
         self.mus = np.asarray(new_mus)
-        self.covs = np.asarray(new_c
+        self.covs = np.asarray(new_covs)
+        
