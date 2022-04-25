e0e56652f2a6aa03231b16265904a314497ff8e7
fanconnie <143478602+fanconnie@users.noreply.github.com>
2022-04-25 18:25:14 +0000

    sysctl: fix null checking in bin_dn_node_address()

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ if __name__ == '__main__':
     image_pixels = np.reshape(image, (-1, image_channels))
     _mean = np.mean(image_pixels,axis=0,keepdims=True)
     _std = np.std(image_pixels,axis=0,keepdims=True)
-    image_pixels = (image_pixels - _mea
+    image_pixels = (image_pixels - _mean) / _std # Nor
