3dce4b42731e1352dd33368c9005a781165a8107
fanconnie <143478602+fanconnie@users.noreply.github.com>
2022-11-18 12:50:14 +0000

    webgl

--- main.py
+++ main.py
@@ -62,3 +62,4 @@ if __name__ == '__main__':
     # Apply K-Means to find the initial weights and covariance matrices for GMM
     kmeans = KMeans(n_clusters=ncomp)
     labels = kmeans.fit_predict(image_pixels)
+    i
