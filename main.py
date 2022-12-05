9ee39be24d688a8eab8064a1b87bc414dab610ac
fanconnie <143478602+fanconnie@users.noreply.github.com>
2022-12-05 18:19:59 +0000

    security: add ioctl specific auditing to lsm_audit

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
     # Apply K-Means to find the initial weights and covariance matrices for GMM
     kmeans = KMeans(n_clusters=ncomp)
     labels = kmeans.fit_predict(image_pixels)
-    initial_mus = kmeans.clu
+    initial_mus = kmeans.cluster_c
