528b4ad452506050c3ef91fcd4017312867fb686
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-03-17 12:26:02 +0000

    hopefully preventing duplicates during connection in and out

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     initial_priors, initial_covs = [], []
     for i in range(ncomp):
         datas = np.array([image_pixels[j, :] for j in range(len(labels)) if labels[j] == i]).T
-     
+        initial_co
