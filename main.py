fa3f5c721858c8fe6213fc4f86621aa92e9f840d
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-05-16 16:41:54 +0000

    Updated the nco feedstock.

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ if __name__ == '__main__':
         print('Iteration {}: Log Likelihood = {}'.format(i+1, log_likelihood))
         if prev_log_likelihood != None and abs(log_likelihood - prev_log_likelihood) < 1e-10:
             break
-        prev_log_likelihood
+        prev_log_likelihood = log_lik
