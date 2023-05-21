13b8f73e9288d6728ac88dc3bb0529450e4e18c8
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-05-21 13:21:17 +0000

    Minor code refactoring

--- main.py
+++ main.py
@@ -84,4 +84,5 @@ if __name__ == '__main__':
 
     # Show Result
     beliefs, log_likelihood = gmm.inference(image_pixels)
-    map_beliefs = np.reshape(beliefs, (image_height, image_width, nco
+    map_beliefs = np.reshape(beliefs, (image_height, image_width, ncomp))
+    segme
