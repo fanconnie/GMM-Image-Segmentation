81949c6f969bce83aa6a28155cf8a4a788143dc9
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-05-22 07:32:22 +0000

    Updated copyright dates

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ if __name__ == '__main__':
     # Show Result
     beliefs, log_likelihood = gmm.inference(image_pixels)
     map_beliefs = np.reshape(beliefs, (image_height, image_width, ncomp))
-    segmented_map = np.
+    segmented_map = np.zeros((imag
