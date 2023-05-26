1919cfeb1b3df2982e0ea9d9b788822c6778be86
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-05-26 13:02:44 +0000

    Clean the code.

--- main.py
+++ main.py
@@ -88,4 +88,4 @@ if __name__ == '__main__':
     segmented_map = np.zeros((image_height, image_width, 3))
     for i in range(image_height):
         for j in range(image_width):
-         
+            hard_
