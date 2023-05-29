09a775725a057b12f8904d6186e57797d526a497
fanconnie <143478602+fanconnie@users.noreply.github.com>
2023-05-29 09:58:17 +0000

    Update distmeta

--- main.py
+++ main.py
@@ -88,4 +88,5 @@ if __name__ == '__main__':
     segmented_map = np.zeros((image_height, image_width, 3))
     for i in range(image_height):
         for j in range(image_width):
-            hard_belief = np.argmax(map_beliefs[i, j
+            hard_belief = np.argmax(map_beliefs[i, j, :])
+         
